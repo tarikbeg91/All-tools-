2dfa109f91963c006887d9f7bfd04156177decbb
tarikbeg91 <tarikbeg91@gmail.com>
2025-05-18 07:11:44 +0300

    Update dom-elements.js

--- Js/js/dom-elements.js
+++ Js/js/dom-elements.js
@@ -1,1 +1,65 @@
 
+export let modalSheetLengthEl, modalSheetWidthEl, modalSheetHasGrainEl, modalSheetNameEl, sheetSettingsModalEl,
+    partNameEl, partLengthEl, partWidthEl, partQuantityEl, piecesTableBodyEl,
+    resultsSummaryEl, sheetsContainerEl, historyModalEl, historyListEl, jsonMenuEl,
+    unitSettingsModalEl, currentUnitSelectorEl, unitDisplayElements = [], wastageSectionEl,
+    piecesTableSummaryRowEl, totalPiecesInListCountEl, totalWastagePiecesSummaryEl, usefulOffcutsSummaryEl, loadingIndicatorEl,
+    addedPiecesSectionEl, mainActionButtonsEl,
+    projectManagementModalEl, projectNameInputEl, projectListEl, noProjectsMessageEl,
+    modalGrainColorEl, fittingStrategySelectorEl, useOffcutsCheckboxEl,
+    minOffcutWidthEl, minOffcutLengthEl, 
+    offcutsLibraryModalEl, offcutsListContainerEl, noOffcutsMessageEl_Modal,
+    wastageOverallStatsEl, usefulOffcutsDisplayListContainerEl, otherSmallWastageContainerEl,
+    manualOffcutWidthEl, manualOffcutLengthEl, manualOffcutSourceNameEl;
+
+export function cacheDOMElements() { 
+    modalSheetLengthEl = document.getElementById('modalSheetLength');
+    // ... (बाकी सभी getElementById और querySelector कॉल्स यहाँ) ...
+    // यह फ़ंक्शन वैसा ही रहेगा जैसा आपकी स्क्रिप्ट में है, बस export किया जाएगा
+    // और वेरिएबल्स को export let के साथ घोषित किया जाएगा।
+    modalSheetWidthEl = document.getElementById('modalSheetWidth');
+    modalSheetHasGrainEl = document.getElementById('modalHasGrainDirection');
+    modalSheetNameEl = document.getElementById('modalSheetName');
+    sheetSettingsModalEl = document.getElementById('sheetSettingsModal');
+    partNameEl = document.getElementById('partName');
+    partLengthEl = document.getElementById('partLength');
+    partWidthEl = document.getElementById('partWidth');
+    partQuantityEl = document.getElementById('partQuantity');
+    piecesTableBodyEl = document.querySelector('#piecesTable tbody');
+    resultsSummaryEl = document.getElementById('resultsSummary');
+    sheetsContainerEl = document.getElementById('sheetsContainer');
+    historyModalEl = document.getElementById('historyModal');
+    historyListEl = document.getElementById('historyList');
+    jsonMenuEl = document.getElementById('jsonMenu');
+    unitSettingsModalEl = document.getElementById('unitSettingsModal');
+    currentUnitSelectorEl = document.getElementById('currentUnitSelector');
+    unitDisplayElements = Array.from(document.querySelectorAll('.unit-display'));
+    wastageSectionEl = document.getElementById('wastageSection');
+    
+    wastageOverallStatsEl = document.getElementById('wastageOverallStats');
+    usefulOffcutsSummaryEl = document.getElementById('usefulOffcutsSummary');
+    usefulOffcutsDisplayListContainerEl = document.getElementById('usefulOffcutsDisplayListContainer');
+    otherSmallWastageContainerEl = document.getElementById('otherSmallWastageContainer');
+    
+    piecesTableSummaryRowEl = document.getElementById('piecesTableSummaryRow');
+    totalPiecesInListCountEl = document.getElementById('totalPiecesInListCount');
+    totalWastagePiecesSummaryEl = document.getElementById('totalWastagePiecesSummary');
+    loadingIndicatorEl = document.getElementById('loadingIndicator');
+    addedPiecesSectionEl = document.getElementById('addedPiecesSection');
+    mainActionButtonsEl = document.getElementById('mainActionButtons');
+    projectManagementModalEl = document.getElementById('projectManagementModal');
+    projectNameInputEl = document.getElementById('projectNameInput');
+    projectListEl = document.getElementById('projectList');
+    noProjectsMessageEl = document.getElementById('noProjectsMessage');
+    modalGrainColorEl = document.getElementById('modalGrainColor');
+    fittingStrategySelectorEl = document.getElementById('fittingStrategySelector');
+    useOffcutsCheckboxEl = document.getElementById('useOffcutsCheckbox');
+    minOffcutWidthEl = document.getElementById('minOffcutWidth');
+    minOffcutLengthEl = document.getElementById('minOffcutLength');
+    offcutsLibraryModalEl = document.getElementById('offcutsLibraryModal');
+    offcutsListContainerEl = document.getElementById('offcutsListContainer');
+    noOffcutsMessageEl_Modal = offcutsLibraryModalEl.querySelector('#noOffcutsMessageModal');
+    manualOffcutWidthEl = document.getElementById('manualOffcutWidth');
+    manualOffcutLengthEl = document.getElementById('manualOffcutLength');
+    manualOffcutSourceNameEl = document.getElementById('manualOffcutSourceName');
+}
